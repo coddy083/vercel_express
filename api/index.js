98c14add201ad2014cb20df47d410de4962fa282
coddy083 <coddy083@gmail.com>
2023-12-21 12:04:37 +0900

    fix

--- api/index.js
+++ api/index.js
@@ -4,4 +4,12 @@ app.get("/api", (req, res) => {
   res.end(`Hello from API`);
 });
 
+app.get("/api/whoami", (req, res) => {
+  const ipaddress =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  res.json({ ipaddress, language, software });
+});
+
 module.exports = app;
